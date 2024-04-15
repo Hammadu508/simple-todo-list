9b61548ca334f5ab6dbb561bd6e7ff1deff804e7
Muhammad Hammad <160263701+Hammadu508@users.noreply.github.com>
2024-04-15 15:54:17 +0500

    simple-todo-list

---
+++ main.ts
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+  let answere = await inquirer.prompt([
+    {
+      name: "select",
+      type: "list",
+      message: "please select operation",
+      choices: ["Add", "view", "Exit"],
+    },
+  ]);
+  if (answere.select === "Add") {
+    let addTodo = await inquirer.prompt({
+      name: "todo",
+      type: "input",
+      message: "Add item in the list",
+    });
+    todos.push(addTodo.todo);
+    console.log(todos);
+  }
+
+  if (answere.select === "view") {
+    console.log("***** TO-DO-LIST******");
+    console.log(todos);
+  }
+  if (answere.select === "Exit") {
+    console.log("Exiting program....");
+    condition = false;
+  }
+}
